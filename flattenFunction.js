2a88bb5b53aeedfd2fa48cc5e9c1d4920d992085
Kajornjit Songsaen <kajornjit@gmail.com>
2017-11-12 14:58:17 +0700

    first commit

---
+++ flattenFunction.js
@@ -0,0 +1,32 @@
+﻿function flatInteger(obj) {
+    var flatIntegerArray = new Array();
+    var localobj = obj;
+
+    if (typeof (localobj) == 'object') {
+        if (localobj.length == 1) {
+            var number = localobj.pop();
+            flatIntegerArray.push(number);
+            return flatIntegerArray;
+        } else {
+            var result = new Array();
+            for (var i = 0; i < localobj.length; i++) {
+                var item = localobj[i];
+                var secondLevel = flatInteger(item);
+                result = result.concat(secondLevel);
+            }
+            return result;
+        }
+    } else if (typeof (obj) != 'number') {
+        flatIntegerArray.push(Number(localobj));
+        return flatIntegerArray;
+    } else {
+        flatIntegerArray.push(localobj);
+        return flatIntegerArray;
+    }
+};
+
+function customFlatten() {
+    var customArray = $('#customArray').val().trim();
+    var result = flatInteger(JSON.parse(customArray)); //JSON.parse('[' + customArray + ']');
+    $('#displayResult').text(JSON.stringify(result));
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+﻿QUnit.test("Arbitraritly Nested Arrays Test", function (assert) {
+    var arbitrarilyNestedArrays = [[1, 2, [3]], 4];
+    var expected = [1, 2, 3, 4];
+    var actual = flatInteger(arbitrarilyNestedArrays);
+    assert.equal(expected, expected, "Expected value: " + expected + " | Actual value: " + expected);
+});
